fa1d2c1e4a94fdbdcc71a563b87e5d7da71a287e
Marios Ouranos <ouranosmarios@outlook.com.gr>
2023-03-06 20:20:18 +0200

    Initial commit

---
+++ src/main/java/com/eshopTest/eshopTest/EshopTestApplication.java
@@ -0,0 +1,13 @@
+package com.eshopTest.eshopTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshopTest/eshopTest/EshopTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshopTest.eshopTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

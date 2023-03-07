23d42d63954acecb646b46fce6babef153b739a0
Hadji Dami <hadjidamianou.damianos@gmail.com>
2023-03-07 11:11:38 +0200

    created two entities and ProductController

---
+++ src/main/java/com/eshopTest/eshopTest/controler/OrderController.java
@@ -0,0 +1,4 @@
+package com.eshopTest.eshopTest.controler;
+
+public class OrderController {
+}
---
+++ src/main/java/com/eshopTest/eshopTest/entity/Order.java
@@ -0,0 +1,85 @@
+package com.eshopTest.eshopTest.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+@Entity
+@Table(name ="orders")
+public class Order {
+    @Id
+
+    private Long orderId;
+    private long quantity;
+    private  long code;
+    private float discount;
+    @OneToMany(mappedBy ="order")
+    private Set<Product> products;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return orderId.equals(order.orderId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", quantity=" + quantity +
+                ", code=" + code +
+                ", discount=" + discount +
+                ", products=" + products +
+                '}';
+    }
+}
